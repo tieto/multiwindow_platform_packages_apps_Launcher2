31e3e8c2aca4699d0aebd5eaaef7bf2a804b08f4
igor.titkov <fit009c@motorola.com>
2013-05-01 11:16:12 -0500

    Explicitly assigning View.NO_ID id to All Apps button to avoid conflict with other views that use 0 as the id.

--- src/com/android/launcher2/Hotseat.java
+++ src/com/android/launcher2/Hotseat.java
@@ -142,6 +142,6 @@ public class Hotseat extends FrameLayout {
         int y = getCellYFromOrder(mAllAppsButtonRank);
         CellLayout.LayoutParams lp = new CellLayout.LayoutParams(x,y,1,1);
         lp.canReorder = false;
-        mContent.addViewToCellLayout(allAppsButton, -1, 0, lp, true);
+        mContent.addViewToCellLayout(allAppsButton, -1, View.NO_ID, lp, true);
     }
 }
