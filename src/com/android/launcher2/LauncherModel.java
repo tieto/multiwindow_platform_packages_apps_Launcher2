2d23e50d1145b8c4d4a5ef8196ce9a38f7b27f92
Justin Stubbs <njd876@motorola.com>
2013-06-05 10:39:01 -0500

    Fixing 'Background loading is already running' error in Launcher2

--- src/com/android/launcher2/LauncherModel.java
+++ src/com/android/launcher2/LauncherModel.java
@@ -1031,7 +1031,16 @@ public class LauncherModel extends BroadcastReceiver {
                 if (mIsLoaderTaskRunning) {
                     // Ensure that we are never running the background loading at this point since
                     // we also touch the background collections
-                    throw new RuntimeException("Error! Background loading is already running");
+                    // If we are in that state, we can't just throw a force close, as it's bad for
+                    // user.Instead we can implement a delayed start to the action.
+                    Log.e(TAG, "catch task happened! initiating wait!");
+                    final int syncBindPage = synchronousBindPage;
+                    sWorker.postDelayed(new Runnable() {
+                        public void run() {
+                            Log.e(TAG, "wait passed, attempting reentry");
+                            runBindSynchronousPage(syncBindPage);
+                        }
+                    }, 200);
                 }
             }
 
